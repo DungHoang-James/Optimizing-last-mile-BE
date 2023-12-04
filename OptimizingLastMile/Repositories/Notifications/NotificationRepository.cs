49b78e8f726bc551d24e3b2a8e21d20256d334c8
DevSupport3 <anhdungit.ni@gmail.com>
2023-12-04 20:26:55 +0700

    fix bug get notification

--- OptimizingLastMile/Repositories/Notifications/NotificationRepository.cs
+++ OptimizingLastMile/Repositories/Notifications/NotificationRepository.cs
@@ -1,4 +1,5 @@
-﻿using OptimizingLastMile.Configs;
+﻿using Microsoft.EntityFrameworkCore;
+using OptimizingLastMile.Configs;
 using OptimizingLastMile.Entites;
 using OptimizingLastMile.Repositories.Base;
 using OptimizingLastMile.Utils;
@@ -16,7 +17,11 @@ public class NotificationRepository : BaseRepository<NotificationLog>, INotifica
 
     public async Task<Pagination<NotificationLog>> GetNotificationPaging(long receiverId, int pageNumber, int pageSize)
     {
-        var query = _dbContext.NotificationLogs.Where(n => n.ReceiverId == receiverId).OrderByDescending(n => n.CreatedDate);
+        var query = _dbContext.NotificationLogs
+            .Include(n => n.Driver).ThenInclude(a => a.DriverProfile)
+            .Include(n => n.Receiver)
+            .Where(n => n.ReceiverId == receiverId)
+            .OrderByDescending(n => n.CreatedDate);
 
         return await Pagination<NotificationLog>.CreateAsync(query, pageNumber, pageSize);
     }
